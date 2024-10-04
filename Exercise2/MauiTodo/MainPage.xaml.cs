2df69e23c4b926ca8217bcb00f63994805372535
ChristianHFLeth <166382485+ChristianHFLeth@users.noreply.github.com>
2024-10-04 20:50:13 +0200

    Added code-behind functionality

--- Exercise2/MauiTodo/MainPage.xaml.cs
+++ Exercise2/MauiTodo/MainPage.xaml.cs
@@ -1,24 +1,46 @@
-﻿namespace MauiTodo;
+﻿using MauiTodo.Models;
+namespace MauiTodo;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	string _toDoListData = string.Empty;
+	readonly Database _database;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		_database = new Database();
+		_ = Initialize();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async Task Initialize()
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		var todos = await _database.GetTodos();
+		foreach (var todo in todos)
+		{
+			_toDoListData += $"{todo.Title} - Due: {todo.Due}\n";
+		}
+		TodosLabel.Text = _toDoListData;
 	}
+
+	async void Button_Clicked(object sender, EventArgs e)
+	{
+		var todo = new TodoItem
+		{
+			Title = TaskEntry.Text,
+			Due = DueDatePicker.Date
+		};
+		var inserted = await _database.Addtodo(todo);
+
+		if(inserted!=0)
+		{
+			_toDoListData += $"{todo.Title} - Due: {todo.Due}\n";
+			TodosLabel.Text = _toDoListData;
+
+			TaskEntry.Text = string.Empty;
+			DueDatePicker.Date = DateTime.Now;
+		}
+	}
+	
 }
 
