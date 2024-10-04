a321cc5c84efa8a0b5a99ded34bcaa18eec00658
ChristianHFLeth <166382485+ChristianHFLeth@users.noreply.github.com>
2024-10-04 20:06:38 +0200

    Added sql packages

--- Exercise2/MauiTodo/obj/Debug/net8.0-ios/iossimulator-arm64/MauiTodo.AssemblyInfo.cs
+++ Exercise2/MauiTodo/obj/Debug/net8.0-ios/iossimulator-arm64/MauiTodo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c99793bb01705cef1ce9c2b1e2f226427a212fc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
